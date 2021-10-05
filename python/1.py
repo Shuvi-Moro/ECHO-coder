82c286ece261e095ea22e6c31ee7b45d75a4c6e3
Shuvi-Moro <63977167+Shuvi-Moro@users.noreply.github.com>
2021-10-05 21:16:33 +0300

    Add files via upload

--- python/1.py
+++ python/1.py
@@ -1,10 +1,4 @@
 import function as func
-txtrw = input ("Напишите текст:\n")
-register =  str (input ("""\nДелать разделение по регистру?
-Пример ==> \"да\" да или 1, \"нет\" нет или 0\n	"""))
-txtrw2 = func.reg (txtrw, register)
-leng = str (input ("""\nНапишите язык на котором это делать сортировку:
-Пример ==> \"русский\" ru или 1, \"ангельский XD\" eng или 2 либо \"кастомную сортировку\" custom или 0\n	"""))
-txtrwlist = list (txtrw2)
-rwtxtpls = func.leng_sort (leng, txtrwlist)
-func.print_info (rwtxtpls, txtrw2)
+wd = str (input ("\nЧто сделать:\nПровести частотный анализ = 0\nШифровка                  = 1\nДешифровка                = 2\n >>> "))
+func.wdoing (wd)
+print ("")
--- python/function.py
+++ python/function.py
@@ -5,6 +5,7 @@ def sings (txtlist):
 	txt = list (txtlist)
 	a = lengs (le)
 	q2 = 0
+	iii = 1
 	while (num >= 1):
 		i = 1
 		ii = 1
@@ -30,18 +31,28 @@ def sings (txtlist):
 							i -= 1
 							ii -= 1
 		else:
-			c = str (txtlist.pop (0))
-			num = len (txtlist)
-			while (i == 1):
+			while (ii == 1):
 				try:
-					txtlist.remove (c)
-				except ValueError:
-					end_list.append (c)
+					c = str (txt2.pop (0))
+				except:
+					с = "XD"
 					i -= 1
+					ii -= 1
+				if (c == b or b == "nope"):
+					while (i == 1):
+						try:
+							txt.remove (c)
+						except ValueError:
+							end_list.append (c)
+							i -= 1
+							ii -= 1
 		num = len (txt)
 		q1 = len (end_list)
 		if (q1 != q2):
-			print (str (len (end_list)) + " знаков готово")
+			if (iii == 1):
+				print ("\n")
+				iii -= 1
+			print (" >>> " + str (len (end_list)) + " знаков готово")
 		q2 = q1
 	return end_list
 
@@ -62,7 +73,7 @@ def leng_info (sing_list, txtlist):
 	i = 0
 	while (manh == 0):
 		try:
-			print ("   " + str (sing_list[i]) + " = " + str (txtlist.count (sing_list[i])))
+			print ("   \" " + str (sing_list[i]) + " \" = " + str (txtlist.count (sing_list[i])))
 			i += 1 
 		except:
 			print ("\n" + "	Разных знаков в тексте = " + str (i))
@@ -103,14 +114,175 @@ def lengs (le):
 	elif (le == "eng"):
 		a = list (" AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzАаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя1234567890")
 	elif (le == "cst"):
-		a = list (str (input ("Напишите порядок символов для сортировки:\n")))
+		a = list (str (input ("Напишите порядок символов для сортировки:\n >>> ")))
 	else:
 		a = []
 	return a
 
-def reg(txtrw, register):
+def reg (txtrw, register):
 	if (register == "нет" or register == "0"):
 		a = txtrw.upper ()
 	else:
 		a = txtrw
 	return a
+
+def wdoing (wd):
+	if (wd == "0"):
+		txtrw = input ("\nНапишите текст:\n >>> ")
+		register =  str (input ("""\nДелать разделение по регистру?
+Пример ==> \"да\" или \"нет\", если пропустить то да\n >>> """))
+		txtrw2 = reg (txtrw, register)
+		leng = str (input ("""\nНапишите язык на котором это делать сортировку:
+Пример ==> \"русский\" ru или 1, \"ангельский XD\" eng или 2 либо \"кастомную сортировку\" custom или 0\n >>> """))
+		txtrwlist = list (txtrw2)
+		rwtxtpls = leng_sort (leng, txtrwlist)
+		print_info (rwtxtpls, txtrw2)
+	elif (wd == "1"):
+		met = input ("\nМетод шифрования:\nAT-BUSH - 0\nЦезарь  - 1\nЗамена  - 2\n >>> ")
+		txtrw = input ("\nНапишите текст:\n >>> ")
+		leng = input ("""\nНа каком языке проводить шифрование:
+Пример ==> \"русский\" rus или 1, \"ангельский XD\" eng или 2 либо \"кастомную сортировку\" custom или 0\n >>> """)
+		if (met == "0"):
+			at_bash_endecoder (txtrw, leng)
+		elif (met == "1"):
+			cezar_encoder (txtrw, leng)
+	elif (wd == "2"):
+		met = input ("\nМетод дешифрования:\nAT-BUSH - 0\nЦезарь  - 1\nЗамена  - 2\n >>> ")
+		txtrw = input ("\nНапишите текст:\n >>> ")
+		leng = input ("""\nНа каком языке проводить шифрование:
+Пример ==> \"русский\" rus или 1, \"ангельский XD\" eng или 2 либо \"кастомную сортировку\" custom или 0\n >>> """)
+		if (met == "0"):
+			at_bash_endecoder (txtrw, leng)
+		elif (met == "1"):
+			knon = input ("\nИзвестно ли смещение?\nЕсли \"да\" то 1 или \"нет\" то 0 или ничего\n >>> ")
+			if (knon == "да" or knon == "1"):
+				cezar_encoder (txtrw, leng)
+			elif (knon == "нет" or knon == "0"):
+				cezar_decoder (txtrw, leng)
+			else:
+				cezar_decoder (txtrw, leng)
+	else:
+		pass
+
+def coder_lengs (le):
+	if (le == "rus" or "1"):
+		a = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+		b = input ("\nНапишите дополнительные знаки для шифратора/дешифратора (в нужном порядке)\nЕсли не требуется ни чего, кроме алфавита, просто нажмите enter\n >>> ")
+		c = b + a
+		return c
+	elif (le == "eng" or "2"):
+		a = list ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+		b = list (input ("\nНапишите дополнительные знаки для шифратора/дешифратора (в нужном порядке)\nЕсли не требуется ни чего, кроме алфавита, просто нажмите enter\n >>> "))
+		c = a.extend (b)
+		return c
+	elif (le == "cst" or "0"):
+		a = list (input ("\nНапишите дополнительные знаки для шифратора/дешифратора (в нужном порядке)\nЕсли надо зашифровать среди символов из текста нажмите enter\n >>> "))
+		return a
+	else:
+		print ( " >>> ERROR " )
+
+def at_bash_endecoder (txtrw, leng):
+	txt = list (txtrw.upper ())
+	text = txt
+	alf = list (coder_lengs (leng))
+	end_list = []
+	try:
+		alf.reverse ()
+		revalf = list (alf)
+		alf.reverse ()
+	except:
+		print ( " >>> ERROR " )
+	i = 1
+	ii = 0
+	space = str (input ("\nУбираем ли пробелы:\nЕсли \"да\" то 1 или \"нет\" то 0 или ничего\n >>> "))
+	print ("")
+	if (space == "да" or space == "1"):
+		while (text.count (" ") >= 1):
+			text.remove (" ")
+	else:
+		pass
+	while (i == 1):
+		text = txt
+		try:
+			poptext = text.pop (ii)
+			try:
+				ind = int (alf.index (poptext))
+				a = revalf.pop (ind)
+				end_list.append (a)
+				revalf.insert (ind, a)
+			except:
+				end_list.append (poptext)
+		except:
+			i -= 1
+	end = ''.join (end_list)
+	print ("\nОтвет:\n >>> " + end)
+
+def cezar_encoder (txtrw, leng):
+	txt = list (txtrw.upper ())
+	text = txt
+	alf = list (coder_lengs (leng))
+	end_list = []
+	i = 1
+	ii = 0
+	space = str (input ("\nУбираем ли пробелы:\nЕсли \"да\" то 1 или \"нет\" то 0 или ничего\n >>> "))
+	delta = int (input ("\nНа какое значение смещать:\nВведите число смещения (1, 2... или -1, -2...)\n >>> "))
+	print ("")
+	if (space == "да" or space == "1"):
+		while (text.count (" ") >= 1):
+			text.remove (" ")
+	else:
+		pass
+	while (i == 1):
+		text = txt
+		alf2 = list (alf)
+		try:
+			poptext = text.pop (ii)
+			try:
+				ind = int (alf2.index (poptext)) + delta
+				if (ind > len (alf2) or ind < 0):
+					if (ind > len (alf2)):
+					 	ind -= len (alf2)
+					elif (ind < 0):
+						ind += len (alf2)
+				a = alf2.pop (ind)
+				end_list.append (a)
+			except:
+				end_list.append (poptext)
+		except:
+			i -= 1
+	end = ''.join (end_list)
+	print ("\nОтвет:\n >>> " + end)
+
+def cezar_decoder (txtrw, leng):
+	txt = list (txtrw.upper ())
+	text = list (txt)
+	alf = list (coder_lengs (leng))
+	end_list = list ("")
+	end_list.clear ()
+	i = 1
+	iii = 0
+	while (iii <= len (alf)):
+		text = list (txt)
+		while (i == 1):
+			alf2 = list (alf)
+			try:
+				poptext = text.pop (0)
+				try:
+					ind = int (alf2.index (poptext)) + iii
+					if (ind > len (alf2) or ind < 0):
+						if (ind > len (alf2)):
+						 	ind -= len (alf2)
+						elif (ind < 0):
+							ind += len (alf2)
+					a = alf2.pop (ind)
+					end_list.append (a)
+				except:
+					end_list.append (poptext)
+			except:
+				i -= 1
+		end = ''.join (end_list)			
+		print ("\nСмещение " + str(iii) + ":\n >>> " + end)
+		iii += 1
+		end_list.clear ()
+		i += 1
+	print ("")
